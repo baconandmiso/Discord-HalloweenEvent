cb273ed89290654e713e273c8ee3557af380195a
Kinoemon <Kinosoft@outlook.jp>
2024-10-27 01:11:44 +0900

    得点の処理

---
+++ DiscordBot-HelloweenEvent/Modules/Event/Point.cs
@@ -0,0 +1,94 @@
+﻿namespace Modules.Event;
+
+/// <summary>
+///     得点の管理をするオブジェクトです。
+/// </summary>
+public class Point
+{
+    /// <summary>
+    ///     ユーザーID
+    /// </summary>
+    public ulong UserId { get; }
+
+    /// <summary>
+    ///     得点
+    /// </summary>
+    public int Score { get; }
+
+    /// <summary>
+    ///     オブジェクトを初期化します。
+    /// </summary>
+    /// <param name="userId">ユーザーID</param>
+    /// <param name="score">得点</param>
+    /// <exception cref="ArgumentException"></exception>
+    public Point(ulong userId, int score)
+    {
+        if (score < 0)
+        {
+            throw new ArgumentException("得点は0以上である必要があります。", nameof(score));
+        }
+
+        UserId = userId;
+        Score = score;
+    }
+
+    /// <summary>
+    ///     加点します。
+    /// </summary>
+    /// <param name="point">加算する得点を持つPointオブジェクト。</param>
+    /// <exception cref="NullReferenceException"></exception>
+    /// <exception cref="ArgumentException"></exception>"
+    public Point Add(Point point)
+    {
+        if (point == null)
+        {
+            throw new NullReferenceException("オブジェクトがnullです。");
+        }
+
+        if (point.UserId != UserId)
+        {
+            throw new ArgumentException("ユーザーIDが一致しません。", nameof(point));
+        }
+
+        if (point.Score < 0)
+        {
+            throw new ArgumentException("得点は0以上である必要があります。", nameof(point));
+        }
+
+        var newPoint = Score + point.Score;
+        return new Point(UserId, newPoint);
+    }
+
+    /// <summary>
+    ///    減点します。
+    /// </summary>
+    /// <param name="point">減算する得点を持つPointオブジェクト。</param>
+    /// <exception cref="NullReferenceException"></exception>
+    /// <exception cref="ArgumentException"></exception>
+    public Point Sub(Point point)
+    {
+        if (point == null)
+        {
+            throw new NullReferenceException("オブジェクトがnullです。");
+        }
+
+        if (point.UserId != UserId)
+        {
+            throw new ArgumentException("ユーザーIDが一致しません。", nameof(point));
+        }
+
+        if (point.Score < 0)
+        {
+            throw new ArgumentException("得点は0以上である必要があります。", nameof(point));
+        }
+
+        var newPoint = Score - point.Score;
+        if (newPoint < 0) // 0未満にならないようにする
+        {
+            newPoint = 0;
+        }
+
+        return new Point(UserId, newPoint);
+    }
+}
+
